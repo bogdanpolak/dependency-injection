7b7daa8309a51fb490cb01b763abd41ef7beef6c
Bogdan Polak <bogdanpol@gmail.com>
2022-06-07 13:48:54 +0200

    Moved demo code into pas unit

--- demos/d05_Factories.dpr
+++ demos/d05_Factories.dpr
@@ -7,97 +7,7 @@
 
 uses
   System.SysUtils,
-  Spring.Container,
-  Spring.Container.Common,
-  Spring;
-
-{$M+}
-
-type
-  IMainService = interface
-    ['{333CAFBE-E9C7-4F1C-9ECA-5F070523007E}']
-    procedure Connect(const aToken: string);
-    procedure Run();
-  end;
-
-  IDbConnection = interface
-    ['{6DCC3C50-A008-437C-8821-7A637DBB9BBA}']
-    procedure ExecuteSql(const aSql: string);
-  end;
-
-type
-  TMainService = class(TInterfacedObject, IMainService)
-  private
-    fConnectionFactory: IFactory<string, IDbConnection>;
-    fToken: string;
-  public
-    constructor Create(const aConnectionFactory
-      : IFactory<string, IDbConnection>);
-    procedure Connect(const aToken: string);
-    procedure Run();
-  end;
-
-  TDbConnection = class(TInterfacedObject, IDbConnection)
-  private
-    fToken: string;
-  public
-    constructor Create(const aToken: string);
-    procedure ExecuteSql(const aSql: string);
-  end;
-
-{$M-}
-  { ---------------------------------------------------------- }
-  { TMainService }
-
-constructor TMainService.Create(const aConnectionFactory
-  : IFactory<string, IDbConnection>);
-begin
-  fConnectionFactory := aConnectionFactory;
-end;
-
-procedure TMainService.Connect(const aToken: string);
-begin
-  fToken := aToken;
-end;
-
-procedure TMainService.Run;
-var
-  connection: IDbConnection;
-  aToken: string;
-begin
-  aToken := fToken;
-  connection := fConnectionFactory(aToken);
-  connection.ExecuteSql('SELECT * FROM table');
-end;
-
-{ ---------------------------------------------------------- }
-{ TDbConnection }
-
-constructor TDbConnection.Create(const aToken: string);
-begin
-  fToken := aToken;
-  writeln(Format('1. Connect to SQL database usnig token "%s"', [fToken]));
-end;
-
-procedure TDbConnection.ExecuteSql(const aSql: string);
-begin
-  writeln(Format('2. Executed SQL: "%s" using token "%s"', [aSql, fToken]));
-end;
-
-{ ---------------------------------------------------------- }
-
-procedure RunDemo();
-var
-  mainService: IMainService;
-begin
-  GlobalContainer.RegisterType<IMainService, TMainService>();
-  GlobalContainer.RegisterType<IDbConnection, TDbConnection>();
-  GlobalContainer.RegisterFactory < IFactory < string, IDbConnection >> ();
-  GlobalContainer.Build;
-  mainService := GlobalContainer.Resolve<IMainService>();
-  mainService.Connect('F8188F61-5A7F');
-  mainService.Run();
-end;
+  DemoWithIFactory in 'DemoWithIFactory.pas';
 
 begin
   try
@@ -106,5 +16,4 @@ begin
     on E: Exception do
       writeln(E.ClassName, ': ', E.Message);
   end;
-
 end.
